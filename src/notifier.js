27c014e9d86b4f11fc08985eed60c2baae2890a6
Cristian Castillo <castillo2310@gmail.com>
2020-04-18 18:40:29 +0200

    New function mustNotifyByEmail in notifier.js which checks if user must be notified by email.

--- src/notifier.js
+++ src/notifier.js
@@ -1,14 +1,18 @@
 const sendMail = require('./email');
 
 /**
- *
+ * Notifies the user with the chosen method
  * @param data
  * @param url
  */
 const notify = (data, url) => {
     console.log('Product '+url+' is available.');
 
-    if( Object.keys(data.email).length ) sendMail(data.email.smtp, data.email.port, data.email.user, data.email.password, url);
+    if( mustNotifyByEmail(data) ) sendMail(data.email.smtp, data.email.port, data.email.user, data.email.password, url);
+};
+
+const mustNotifyByEmail = (data) => {
+    return data.email && Object.keys(data.email).length;
 };
 
 module.exports = notify;
