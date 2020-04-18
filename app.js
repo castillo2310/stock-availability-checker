690623d94cb05744985b63f0e95f80d4fb9e47fa
Cristian Castillo <castillo2310@gmail.com>
2020-04-18 16:52:38 +0200

    New notifier.js file with notify function. App.js will call notify() if a product is available.

--- app.js
+++ app.js
@@ -2,22 +2,34 @@
 const getProductId = require('./src/urlParser');
 const isProductAvailable = require('./src/productAvailability');
 const requestUserData = require('./src/dataRequester');
+const notify = require('./src/notifier');
+
+/**
+ * TODO:
+ * 1.- Send notification when a product is available.
+ * 2.- Loop until all products are available or execution is stopped.
+ * 3.- Stop requesting availability of a product if already is available.
+ * 4.- User can provide a file with the required data instead of using the prompt (by argv).
+ */
 
 try {
 
-    requestUserData().then((data) => {
+    requestUserData().then((userData) => {
 
-        if( !data.productUrl.length ) return console.log('At least one product url is required.');
+        if( !userData.productUrl.length ) return console.log('At least one product url is required.');
 
-        for(let i=0;i<data.productUrl.length;i++){
+        for(let i=0;i<userData.productUrl.length;i++){
             try {
-                let productUrl = data.productUrl[i];
+                let productUrl = userData.productUrl[i];
                 let productId = getProductId(productUrl);
                 if (!productId) throw new Error('ProductId not found.');
 
                 const stockUrl = 'https://www.elcorteingles.es/api/stock?products=' + productId;
                 isProductAvailable(stockUrl).then((available) => {
-                    console.log('Product available: ', available);
+
+                    if( available ){
+                        notify(userData.notification, productUrl);
+                    }
                 });
             }catch (e) {
                 console.log('Error: ', e);
---
+++ src/notifier.js
@@ -0,0 +1,14 @@
+const sendMail = require('./email');
+
+/**
+ *
+ * @param data
+ * @param url
+ */
+const notify = (data, url) => {
+    console.log('Product '+url+' is available.');
+
+    if( Object.keys(data.email).length ) sendMail(data.email.smtp, data.email.port, data.email.account, data.email.password, url);
+};
+
+module.exports = notify;
