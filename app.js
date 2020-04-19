d78cd9ea7d4406b73b55012024437ece2d5cccca
Cristian Castillo <castillo2310@gmail.com>
2020-04-19 21:08:20 +0200

    urlParser.js has been removed. Now productAvailability.js gets productId from productUrl and checks its availability.

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-
-const getProductId = require('./src/urlParser');
 const isProductAvailable = require('./src/productAvailability');
 const setConfigData = require('./src/userDataRequester');
 const notify = require('./src/notifier');
 const configManager = require('./src/configManager');
-
-try {
 
     setConfigData().then(() => {
         console.log('Starting to check availability...');
@@ -19,27 +15,20 @@ try {
             }
 
             for(let i=0;i<soldOutProducts.length;i++){
-                try {
-                    let productUrl = soldOutProducts[i];
-                    let productId = getProductId(soldOutProducts[i]);
-                    if (!productId) throw new Error('ProductId not found.');
-
-                    const stockUrl = 'https://www.elcorteingles.es/api/stock?products=' + productId;
-                    isProductAvailable(stockUrl).then((available) => {
-                        if( available ){
-                            configManager.setProductAvailable(productUrl).then(() => {
-                                notify(configManager.getNotificationData(), productUrl);
-                            });
-                        }
-                    });
-                }catch (e) {
-                    console.log('Error: ', e);
-                }
+                let productUrl = soldOutProducts[i];
+                isProductAvailable(productUrl).then((available) => {
+                    if( available ){
+                        configManager.setProductAvailable(productUrl).then(() => {
+                            notify(configManager.getNotificationData(), productUrl);
+                        });
+                    }
+                }).catch((error) => {
+                    console.log('Error: ', error);
+                });
             }
 
         }, 30000);
+    }).catch((error) => {
+        console.log('Error', error);
     });
 
-}catch (e) {
-    console.log('Error: ', e);
-}
--- src/productAvailability.js
+++ src/productAvailability.js
@@ -2,20 +2,47 @@ const request = require('request-promise-native');
 
 /**
  * Does the request and returns a promise with boolean availability value
- * @param url
+ * @param productUrl
  * @returns {Promise<boolean>}
  */
-const isProductAvailable = async (url) => {
-
+const isProductAvailable = async (productUrl) => {
     return new Promise((resolve, reject) => {
-        request(url, {json: true}).then( (result) => {
-            const available = result && result.ADD ? true : false;
+
+        let productId = getProductId(productUrl);
+        if (!productId) reject('ProductId not found.');
+
+        const stockUrl = 'https://www.elcorteingles.es/api/stock?products=' + productId;
+        request(stockUrl, {json: true}).then( (result) => {
+            const available = result.ADD ? true : false;
             resolve( available );
         }).catch((error) => {
             reject(error);
         });
-    });
 
+    });
+};
+
+/**
+ * Parses the url to get the product id
+ * @param url
+ * @returns {string}
+ */
+const getProductId = (url) => {
+    try {
+        let slashPosition = url.lastIndexOf('/');
+        if ( !url[slashPosition + 1] ) slashPosition = url.substring(0, slashPosition).lastIndexOf('/');
+        if( slashPosition === -1 ) throw new Error('SlashPosition not found.');
+
+        const productQueryString = url.substring(slashPosition + 1);
+
+        const hyphenPosition = productQueryString.indexOf('-');
+        if( hyphenPosition === -1 ) throw new Error('HyphenPosition not found.');
+
+        return productQueryString.substring(0, hyphenPosition);
+    }catch (e) {
+        console.log('UrlParseError. Error: ',e);
+        console.log('UrlParseError. URL: ',url)
+    }
 };
 
 module.exports = isProductAvailable;
