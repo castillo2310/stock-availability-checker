6b7fe76c3fa84dd858883f403535d4764f33bcaf
Cristian Castillo <castillo2310@gmail.com>
2020-04-23 20:35:31 +0200

    Now app.js uses setTimeOut instead of setInterval.

--- app.js
+++ app.js
@@ -1,35 +1,37 @@
-const checkAvailability = require('./src/Application/ProductAvailabilityChecker');
+const checkProductAvailability = require('./src/Application/ProductAvailabilityChecker');
 const setConfigData = require('./src/userDataRequester');
 const notify = require('./src/Application/Notifier');
 const configManager = require('./src/configManager');
 
-    setConfigData().then(() => {
-        console.log('Starting to check availability...');
+const delay = 30000;
 
-        const products =  configManager.getProducts();
-        const notificationData = configManager.getNotificationData();
-        let interval = setInterval(() => {
+setConfigData().then(() => {
+    console.log('Starting to check availability...');
 
-            let soldOutProducts = products.filter(p => !p.available);
-            if( !soldOutProducts.length ){
-                clearInterval(interval);
-                return console.log('All products are available. Script ends.')
-            }
+    const products =  configManager.getProducts();
+    const notificationData = configManager.getNotificationData();
 
-            for(let i=0;i<soldOutProducts.length;i++){
-                let product = soldOutProducts[i];
-                checkAvailability(product).then((isAvailable) => {
-                    if (isAvailable) {
-                        product.available = true;
-                        notify(notificationData, product);
-                    }
-                }).catch((error) => {
-                    console.log('Error: ', error);
-                });
-            }
+    (function checkStockAvailability(){
 
-        }, 30000);
-    }).catch((error) => {
-        console.log('Error', error);
-    });
+        let soldOutProducts = products.filter(p => !p.available);
+        if (!soldOutProducts.length) return console.log('All products are available. Script ends.');
+
+        for(let i=0;i<soldOutProducts.length;i++){
+            let product = soldOutProducts[i];
+            checkProductAvailability(product).then((isAvailable) => {
+                if (isAvailable) {
+                    product.available = true;
+                    notify(notificationData, product);
+                }
+            }).catch((error) => {
+                console.log('Error: ', error);
+            });
+        }
+
+        setTimeout(checkStockAvailability, delay);
+    })();
+
+}).catch((error) => {
+    console.log('Error', error);
+});
 
